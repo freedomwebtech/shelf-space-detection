172eb4959b804598265223553fa36246cd0f412d
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-09-01 15:03:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,15 +10,7 @@ def RGB(event, x, y, flags, param):
         point = [x, y]
         print(point)
        
-def drawing(event,x,y,flags,param):
-    if event==cv2.EVENT_LBUTTONDOWN:
-       points.append((x,y))
-       with open("shelf",'wb')as f:
-            pickle.dump(points,f)
-    if event==cv2.EVENT_RBUTTONDOWN:
-       for i,pts in enumerate(points):
-           x1,y1=pts
-           print(i)
+
           
     
         
@@ -32,7 +24,6 @@ while True:
     frame=cv2.resize(frame,(1020,800))
 
     cv2.imshow("RGB",frame)
-#    cv2.setMouseCallback("FRAME",drawing)
     if cv2.waitKey(0) & 0xFF == 27:
         break
 cap.release()    
